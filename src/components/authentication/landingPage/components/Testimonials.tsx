a1039b54323f878431334ccd39a54ac48a1295b5
Preethi V Hiremath <preethivhiremath.vh@gmail.com>
2021-09-07 12:03:56 +0530

    testimonials added

--- src/components/authentication/landingPage/components/Testimonials.tsx
+++ src/components/authentication/landingPage/components/Testimonials.tsx
@@ -17,13 +17,16 @@ function Testimonials() {
                    <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="section-testimonials__map__image"  alt=""/> 
                    <p >
                        You interact very well with the listeners. We don't feel bored at all. We would love to attend more sessions like these in the future.
+                       <p style={{marginTop:'1rem',textAlign:'right'}}>-Vibha Shanbhag ,BMSCE</p>
                    </p>
+                  
                 </div>
 
                 <div className="section-testimonials__map__testimonial2">
                    <img src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="section-testimonials__map__image"  alt=""/> 
                    <p>
                    It was a perfect explanation of a very interesting topic and hands-off to your patience sir.
+                   <p style={{marginTop:'1rem',textAlign:'right'}}>-Shreyas P,DSCE</p>
                    </p>
                 </div>
 
@@ -31,49 +34,57 @@ function Testimonials() {
                    <img src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="section-testimonials__map__image"  alt=""/> 
                    <p>
                    Sir was very interactive and cleared all our doubts. So,there is no improvement that i can suggest for this session. 
+                   <p style={{marginTop:'1rem',textAlign:'right'}}>-Kishan S Murthy,RVCE</p>
                    </p>
                 </div>
 
                 <div className="section-testimonials__map__testimonial4">
                    <img src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="section-testimonials__map__image"  alt=""/> 
                    <p>
-                   The webinar was perfectly carried out by the instructor ,so i don't think any improvisations are required
+                   The webinar was perfectly carried out by the instructor ,so i don't think any improvisations are required.
+                   <p style={{marginTop:'1rem',textAlign:'right'}}>-Raksha A R,NIE</p>
                    </p>
                 </div>
                 <div className="section-testimonials__map__testimonial5">
                    <img src="https://images.unsplash.com/photo-1489980557514-251d61e3eeb6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjF8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="section-testimonials__map__image"  alt=""/> 
                    <p>
-                   Sir explained everything from scratch even we are not from ece or electronic branch we understand it and love it it 
+                   Sir explained everything from scratch even we are not from ece or electronic branch we understand it and love it!
+                   <p style={{marginTop:'1rem',textAlign:'right'}}>-Jay Yadav,RNSIT</p>
                    </p>
                 </div>
                 <div className="section-testimonials__map__testimonial6">
                    <img src="https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="section-testimonials__map__image"  alt=""/> 
                    <p>
                    No improvement needed!! The webinar was too good. Looking forward to many more such events!
+                   <p style={{marginTop:'1rem',textAlign:'right'}}>-Shaik Fazuluddin,BMSCE</p>
                    </p>
                 </div>
                 <div className="section-testimonials__map__testimonial7">
                    <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="section-testimonials__map__image"  alt=""/> 
                    <p>
                    It was a very informative session, had a great experience!
+                   <p style={{marginTop:'1rem',textAlign:'right'}}>-Sanjana Hoskote,BMSCE</p>
                    </p>
                 </div>
                 <div className="section-testimonials__map__testimonial8">
                    <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="section-testimonials__map__image"  alt=""/> 
                    <p>
-                   I really loved the way it was explained.  Very easy to understand.  
+                   I really loved the way it was explained.Very easy to understand!  
+                   <p style={{marginTop:'1rem',textAlign:'right'}}>-Diya M Shubbaiah,JSS STU</p>
                    </p>
                 </div>
                 <div className="section-testimonials__map__testimonial9">
                    <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="section-testimonials__map__image"  alt=""/> 
                    <p>
                    You guys are doing great.Keep it up!! Looking forward to more such sessions!
+                   <p style={{marginTop:'1rem',textAlign:'right'}}>-Afreen R Fathima,RNSIT</p>
                    </p>
                 </div>
                 <div className="section-testimonials__map__testimonial10">
                    <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="section-testimonials__map__image"  alt=""/> 
                    <p>
-                   Sir was very interactive and cleared all our doubts. So,there is no improvement that i can suggest for this session. 
+                   No improvements.I wish we can have more and more informative sessions like this! 
+                   <p style={{marginTop:'1rem',textAlign:'right'}}>-Archana K,PESU</p>
                    </p>
                 </div>
             </div>
